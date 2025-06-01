da4d3db44f7937d980577a7df45f83d5568e019e
Anany Koranne <ananykoranne2609@gmail.com>
2025-06-01 13:26:10 +0530

    Add PUT endpoint for updating skill

--- Backend/routes/skillRoutes.js
+++ Backend/routes/skillRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getAllSkills, addSkill } from '../controllers/skillController.js';
+import { getAllSkills, addSkill, updateSkill } from '../controllers/skillController.js';
 
 const router = express.Router();
 
 router.get('/', getAllSkills);
-router.post('/', addSkill); 
+router.post('/', addSkill);
+router.put('/:id', updateSkill); 
 
 export default router;
